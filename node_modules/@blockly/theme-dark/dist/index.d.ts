a3f3012e0cd93b9900d179c1636ddedc73886d64
Maribeth Moffatt <maribethb@google.com>
2025-09-19 13:27:11 -0700

    rebuild ghp for testing

---
+++ node_modules/@blockly/theme-dark/dist/index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * @fileoverview Dark theme.
+ */
+import * as Blockly from 'blockly/core';
+/**
+ * Dark theme.
+ */
+declare const _default: Blockly.Theme;
+export default _default;
+//# sourceMappingURL=index.d.ts.map
