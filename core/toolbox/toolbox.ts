c3b72ddf7ca433e64a24bb52d1b788f521efaad4
Maribeth Moffatt <maribethb@google.com>
2025-10-02 14:37:20 -0700

    fix: dont manually set posinset for toolbox categories

--- core/toolbox/toolbox.ts
+++ core/toolbox/toolbox.ts
@@ -369,7 +369,6 @@ export class Toolbox
     this.renderContents_(toolboxDef['contents']);
     this.position();
     this.handleToolboxItemResize();
-    this.recomputeAriaOwners();
   }
 
   /**
@@ -446,7 +445,6 @@ export class Toolbox
         this.addToolboxItem_(child);
       }
     }
-    this.recomputeAriaOwners();
   }
 
   /**
@@ -1146,32 +1144,6 @@ export class Toolbox
     if (!nextTree || nextTree !== this.flyout?.getWorkspace()) {
       this.autoHide(false);
     }
-  }
-
-  /**
-   * Recomputes ARIA tree ownership relationships for all of this toolbox's
-   * categories and items.
-   *
-   * This should only be done when the toolbox's contents have changed.
-   */
-  recomputeAriaOwners() {
-    const focusable = this.getFocusableElement();
-    const selectableChildren =
-      this.getToolboxItems().filter((item) => item.isSelectable()) ?? null;
-    const focusableChildElems = selectableChildren.map((selectable) =>
-      selectable.getFocusableElement(),
-    );
-    const focusableChildIds = focusableChildElems.map((elem) => elem.id);
-    aria.setState(
-      focusable,
-      aria.State.OWNS,
-      [...new Set(focusableChildIds)].join(' '),
-    );
-    // Ensure children have the correct position set.
-    // TODO: Fix collapsible subcategories. Their groups aren't set up correctly yet, and they aren't getting a correct accounting in top-level toolbox tree.
-    focusableChildElems.forEach((elem, index) =>
-      aria.setState(elem, aria.State.POSINSET, index + 1),
-    );
   }
 }
 
