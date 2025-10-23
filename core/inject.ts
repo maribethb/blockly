412f17f6ff124bd89d94e67fafbe03eb65ef0748
Maribeth Moffatt <maribethb@google.com>
2025-10-23 16:15:53 -0700

    fix: make the aria live region assertive

--- core/inject.ts
+++ core/inject.ts
@@ -86,7 +86,7 @@ export function inject(
   const ariaAnnouncementSpan = document.createElement('span');
   ariaAnnouncementSpan.id = 'blocklyAriaAnnounce';
   dom.addClass(ariaAnnouncementSpan, 'hiddenForAria');
-  aria.setState(ariaAnnouncementSpan, aria.State.LIVE, 'polite');
+  aria.setState(ariaAnnouncementSpan, aria.State.LIVE, 'assertive');
   subContainer.appendChild(ariaAnnouncementSpan);
 
   return workspace;
